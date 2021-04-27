8fe78a1e7d2899cf72d6587b8716d5c1603f2b37
abhi <abhijeetvc7@gmail.com>
2021-04-27 10:35:15 +0530

    Initial commit

---
+++ src/main/java/com/springmvcproject/spmvcprj/controller/MyController.java
@@ -0,0 +1,29 @@
+package com.springmvcproject.spmvcprj.controller;
+
+import com.springmvcproject.spmvcprj.model.Employee;
+import com.springmvcproject.spmvcprj.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+// @RestController = @Controller + @ResponseBody
+
+@Controller
+public class MyController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @GetMapping("/index")
+    public String check(Model model){
+        model.addAttribute("employee",new Employee());
+        return "index";
+    }
+
+    @PostMapping("/saveEmployee")
+    public String saveEmployee(@ModelAttribute Employee employee){
+        employeeRepository.save(employee);
+        return "welcome";
+    }
+}
---
+++ src/main/java/com/springmvcproject/spmvcprj/repository/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package com.springmvcproject.spmvcprj.repository;
+
+import com.springmvcproject.spmvcprj.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
+}
